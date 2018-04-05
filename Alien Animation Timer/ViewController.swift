d159e16c55fc701719c681825f93a722c92c67f7
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-04-05 15:22:58 +0900

    A

--- Alien Animation Timer/ViewController.swift
+++ Alien Animation Timer/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var counter = 1
+    var myTimer = Timer()
+    
+    @IBOutlet weak var ImageV: UIImageView!
+    
+    @IBOutlet weak var Lab: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Lab.text = String(counter)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func BtS(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
     }
-
-
+    
+    @IBAction func BtF(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @objc func doAnimation() {
+        if counter == 5 {
+            counter = 1
+        } else {
+            counter = counter + 1
+        }
+        ImageV.image = UIImage(named: "frame\(counter).png")
+        Lab.text = String(counter)
+    }
+    
 }
-
